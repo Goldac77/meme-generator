624dad834eb8240e614a73e060a12032a9f6d474
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2022-12-12 18:38:00 +0000

    added api data sample

---
+++ src/memesData.js
@@ -0,0 +1,33 @@
+const memesData = {
+    "success": true,
+    "data": {
+        "memes": [
+            {
+                "id": "181913649",
+                "name": "Drake Hotline Bling",
+                "url": "https://i.imgflip.com/30b1gx.jpg",
+                "width": 1200,
+                "height": 1200,
+                "box_count": 2
+            },
+            {
+                "id": "87743020",
+                "name": "Two Buttons",
+                "url": "https://i.imgflip.com/1g8my4.jpg",
+                "width": 600,
+                "height": 908,
+                "box_count": 3 
+            },
+            {
+                "id": "112067320",
+                "name": "Distracted boyfriend",
+                "url": "https://i.imgflip.com/1upzp4.jpg",
+                "width": 1200,
+                "height": 707,
+                "box_count": 3
+            }
+        ]
+    }
+}
+
+export default memesData
